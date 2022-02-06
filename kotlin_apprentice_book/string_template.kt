0dcc405ea0146fe5456fc862316361f5303aec96
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2022-02-06 18:51:32 +0530

    Update string_template.kt

--- kotlin_apprentice_book/string_template.kt
+++ kotlin_apprentice_book/string_template.kt
@@ -1,5 +1,7 @@
 /* 
 * string template
+* $var and ${expr}
+* supports expression
 */
 
 // import statements
@@ -8,7 +10,12 @@ fun getMsg(name: String, age: Int) : String {
     return "hi my name is $name and age is $age"
 }
 
+fun getExprAdditionMsg(a: Int, b: Int): String {
+    return "the sum of $a and $b is ${a+b}"
+}
+
 fun main() {
     val msg = getMsg("rajat",33)
 	println(msg) // hi my name is rajat and age is 33
+	println(getExprAdditionMsg(3,5))
 }

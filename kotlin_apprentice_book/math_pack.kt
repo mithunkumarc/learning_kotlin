cf92bef6bdcf29ee9d37f8478aa33f4086e813cd
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2022-02-06 07:36:22 +0530

    Create math_pack.kt

---
+++ kotlin_apprentice_book/math_pack.kt
@@ -0,0 +1,13 @@
+/* 
+* https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.math/
+*/
+
+import kotlin.math.*
+
+fun main() {	
+	val i = -100.0200 // | X | : real value, ignores sign
+    println(max(50,max(60, max(70,80)))) // max number of 50, 60, 70 , 80
+   	println(i.absoluteValue)
+    // todo : explore other functions 
+}
+

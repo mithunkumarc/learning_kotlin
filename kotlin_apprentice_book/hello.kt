7f82ca47a2d1e4f7048b660de8b068038109fff0
ಮಿಥುನ್ <mithunchannappa@gmail.com>
2022-02-06 06:49:06 +0530

    Create hello.kt

---
+++ kotlin_apprentice_book/hello.kt
@@ -0,0 +1,8 @@
+/* 
+*  When you run, the Kotlin compiler will parse your code and convert the code to
+*  bytecode, and it will run it on your local JVM
+*/
+fun main() {	
+    println("hello kotlin")
+}
+
